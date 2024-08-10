25430aefcab9c71580bc2fababacb6aed4b5f156
Muhammad Russell <muhammadrussell39@gmail.com>
2024-08-10 09:59:14 +0600

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const imageLinks = [
+  "https://images.clickfunnels.com/ee/2ce11f35fc4ab8898c56d177b3d445/IMG_6595.jpg",
   "https://images.clickfunnels.com/23/3ad895e4e24c4faaa0303b37bdaa8d/IMG_6586.jpg",
   "https://images.clickfunnels.com/73/c0bb80849141168ada4210be3ebf11/IMG_1960.jpg",
-  "https://images.clickfunnels.com/ee/2ce11f35fc4ab8898c56d177b3d445/IMG_6595.jpg",
   "https://images.clickfunnels.com/07/f775419e294293a2da80e6058631d2/IMG_6598.jpg",
   "https://images.clickfunnels.com/81/3459ce1a4641ff82b55f9283f539df/IMG_6583.jpg",
 ];
