0292c5603c89d1af7ae6d2a2c4f3c294e426d5a2
Muhammad Russell <muhammadrussell39@gmail.com>
2024-08-12 10:32:50 +0600

    Add all sliders.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,46 @@
+/**
+ * Scroll Slider
+ */
+
+const scrollSliderDesktop = new Swiper(".swiper-scroll-slider", {
+  spaceBetween: 40,
+  slidesPerView: 1,
+  loop: true,
+  speed: 6000,
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+/**
+ * Testimonial Slider
+ */
+
 const imageLinks = [
-  "https://images.clickfunnels.com/73/c0bb80849141168ada4210be3ebf11/IMG_1960.jpg",
+  "https://images.clickfunnels.com/81/3459ce1a4641ff82b55f9283f539df/IMG_6583.jpg",
   "https://images.clickfunnels.com/23/3ad895e4e24c4faaa0303b37bdaa8d/IMG_6586.jpg",
   "https://images.clickfunnels.com/ee/2ce11f35fc4ab8898c56d177b3d445/IMG_6595.jpg",
   "https://images.clickfunnels.com/07/f775419e294293a2da80e6058631d2/IMG_6598.jpg",
-  "https://images.clickfunnels.com/81/3459ce1a4641ff82b55f9283f539df/IMG_6583.jpg",
+  "https://images.clickfunnels.com/73/c0bb80849141168ada4210be3ebf11/IMG_1960.jpg",
 ];
 
-const swiperWrapper = document.querySelector(".testimonials .swiper-wrapper");
+const testimonialsWrapper = document.querySelector(
+  ".testimonials .swiper-wrapper"
+);
 imageLinks.forEach((link) => {
   const slide = document.createElement("div");
   slide.className = "swiper-slide";
   slide.innerHTML = `<img src="${link}" alt="Slide image" style="width: 100%;">`;
-  swiperWrapper.appendChild(slide);
+  testimonialsWrapper.appendChild(slide);
 });
 
-const testimonialsSlider = new Swiper(".testimonials", {
+const testimonialSlide = new Swiper(".testimonials", {
   effect: "cards",
   grabCursor: true,
   speed: 500,
@@ -28,6 +54,8 @@ const testimonialsSlider = new Swiper(".testimonials", {
     prevEl: ".swipe-prev",
   },
 });
+
+// Video and Multi Slide
 
 const videoSlide = new Swiper(".swiper-video-slider", {
   navigation: {
